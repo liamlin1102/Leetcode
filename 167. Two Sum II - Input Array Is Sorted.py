608e267adec639c6bac237096e62b8a65a4740ee
liamlin <linliam1102@gmail.com>
2023-09-20 22:37:54 +0800

    no message

--- 167. Two Sum II - Input Array Is Sorted.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        dic = {}
-        for index,num in enumerate(numbers):
-            # 如果那個值在裡面，那就回傳
-            if (target-num in dic):
-                return[dic[target-num],index+1]
-            # 如果沒有就加進map
-            dic[num]=index+1
