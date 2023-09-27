db79ff83232c5f41e416d3ab0e5eb685ae7afe83
liamlin <linliam1102@gmail.com>
2023-09-27 23:50:23 +0800

    20230927

---
+++ C++/自己搞定/Easy/125. Valid Palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    char ToLower(char word){
+        if(word>='A'&& word<='Z'){
+            word =word -'A'+'a';
+            }
+        return word;
+    }
+    bool checkWord(char word){
+        return (word>='a' && word<='z')||(word-'0'>=0 && word-'0'<=9);
+    }
+    bool isPalindrome(string s) {
+        if(s.size()<=1){
+            return true;
+        }
+        int left =0; int right = s.size()-1;bool answer = false;
+        while (right>left){
+            char leftWord= ToLower(s[left]);
+            char rightWord= ToLower(s[right]);    
+            if(!checkWord(rightWord)){
+                right--;
+            }
+            else if(!checkWord(leftWord)){
+                left++;
+            }
+            else{         
+                if (leftWord!=rightWord){
+                    return false;
+                }
+                else{
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return true;
+    }
+};
