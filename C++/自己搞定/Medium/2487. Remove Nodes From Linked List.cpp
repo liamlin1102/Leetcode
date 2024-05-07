4c8a474eef6e54cd2532dc6a9425496cfe629782
liamlin <linliam1102@gmail.com>
2024-05-08 02:25:33 +0800

    feat : LC 2816

--- C++/自己搞定/Medium/2487. Remove Nodes From Linked List.cpp
+++ C++/自己搞定/Medium/2487. Remove Nodes From Linked List.cpp
@@ -5,6 +5,15 @@
 #include <vector>
 using namespace std;
 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
